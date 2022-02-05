bc1bd3a57092daf8ff5f87d88cab79b89761c82f
Ivorysanna <violetta.pyralov@web.de>
2022-02-05 16:19:37 +0100

    Index in src Ordner verschoben

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { CarManager } from "./src/CarManager";
-import { Control } from "./src/Control";
-import { Car } from "./src/Car";
+import { CarManager } from "./CarManager";
+import { Control } from "./Control";
+import { Car } from "./Car";
 let control: Control = new Control();
 
 //console.log(CarManager.getInstance().ListOfAvailableCars()[0].printString());
