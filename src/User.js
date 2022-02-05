fd7e254806b56fb451b6f87027eccee51db2bdac
Ivorysanna <violetta.pyralov@web.de>
2022-02-05 18:11:34 +0100

    Js Dateien hinzugefügt

---
+++ src/User.js
@@ -0,0 +1,32 @@
+"use strict";
+exports.__esModule = true;
+exports.User = void 0;
+var fs = require("fs");
+var User = /** @class */ (function () {
+    function User(user_ID, userName, lastName, password) {
+        this.user_ID = user_ID;
+        this.userName = userName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+    User.prototype.pastBookedRides = function () {
+        var allRides = fs.readFileSync("data/rides.json");
+        var outputRides = JSON.parse(allRides.toString());
+        //console.log(outputRides);
+        return "Bis jetzt wurden: " + outputRides + " gebucht.";
+    };
+    //Average Cost wird berechnet
+    User.prototype.averageCost = function () {
+        var rawData = fs.readFileSync("data/rides.json");
+        var allRides = JSON.parse(rawData.toString());
+        var bookedRides = allRides["bookedRides"];
+        var logAllRides = 0;
+        for (var i = 0; i < bookedRides.length; i++) {
+            logAllRides += bookedRides[i].price;
+        }
+        var averageCost = logAllRides / bookedRides.length;
+        return "Durchschnittskosten betragen: " + averageCost + " €.";
+    };
+    return User;
+}());
+exports.User = User;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var Control_1 = require("./Control");
+var control = new Control_1.Control();
+//console.log(CarManager.getInstance().ListOfAvailableCars()[0].printString());
+control.main();
