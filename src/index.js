17a7d08729c1172420fabeadd931c53e6d594434
Ivorysanna <violetta.pyralov@web.de>
2022-02-06 17:08:15 +0100

    js Datien verschon bin dist

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Control_1 = require("./Control");
-var control = new Control_1.Control();
-//console.log(CarManager.getInstance().ListOfAvailableCars()[0].printString());
-control.main();
